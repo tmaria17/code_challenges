9c909f4dfcefd767907e547f54e2d23b3166401e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-06-10 22:46:18 -0600

    Complete panagram.

---
+++ code_wars/panagram.rb
@@ -0,0 +1,12 @@
+require 'pry'
+# A pangram is a sentence that contains every single letter of the alphabet at
+#least once. For example, the sentence "The quick brown fox jumps over the lazy dog"
+#is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+# Given a string, detect whether or not it is a pangram. Return True if it is,
+#False if not. Ignore numbers and punctuation.
+def panagram?(string)
+  alphabet = ('a'..'z').to_a
+  alphabet.all? { |letter|  string.downcase.include?(letter) }
+ end
+
+puts panagram?("The quick brown fox jumps over the lazy dog.")
